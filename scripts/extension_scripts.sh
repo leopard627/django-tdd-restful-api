d476cd101fb184e9b3098ca01c2ebd529ea9f13d
elastic <elastic7327@gmail.com>
2017-06-04 18:41:30 +0900

    for django extensions

---
+++ scripts/extension_scripts.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# cd ../src && python3 manage.py show_urls --settings=superapi.settings.developments
+
+cd ../src && python3 manage.py shell_plus --settings=superapi.settings.developments
+
